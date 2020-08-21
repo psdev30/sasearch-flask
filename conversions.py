8bb4b973a3ed7f2c4b73fbd24673bed500ff2169
pszdev30 <pszjukalkar30@gmail.com>
2020-08-20 20:23:05 -0400

    changes

--- conversions.py
+++ conversions.py
@@ -1,33 +1,33 @@
-from moviepy.editor import *
-from pydub import AudioSegment
-import speech_recognition as sr
-
-class Conversions:
-
-    def convertToMp3(fileName):
-        mp4 = 'clips_library/' + fileName + '.mp4'
-        mp3 = fileName + '.mp3'
-        wav = fileName + '.wav'
-        videoClip = VideoFileClip(mp4)
-        audioClip = videoClip.audio
-        audioClip.write_audiofile(mp3)
-        audioClip.close()
-        videoClip.close()
-        return wav, mp3
-
-    def convertToWav(wav, mp3):
-        filepath = os.path.abspath(mp3)
-        sound = AudioSegment.from_mp3(filepath).export(wav, format="wav")
-
-    def extractText(wav, fileName):
-        r = sr.Recognizer()
-        with sr.WavFile(os.path.abspath(wav)) as source:
-            audio = r.record(source)
-            text = r.recognize_google(audio)
-            name = fileName.title()
-            if ('_' in fileName):
-                name = name.replace('_', ' ')
-            short_path = fileName + '.mp4'
-        os.remove(fileName + '.mp3')
-        os.remove(fileName + '.wav')
-        return name, short_path, text
+# from moviepy.editor import *
+# from pydub import AudioSegment
+# import speech_recognition as sr
+#
+# class Conversions:
+#
+#     def convertToMp3(fileName):
+#         mp4 = 'clips_library/' + fileName + '.mp4'
+#         mp3 = fileName + '.mp3'
+#         wav = fileName + '.wav'
+#         videoClip = VideoFileClip(mp4)
+#         audioClip = videoClip.audio
+#         audioClip.write_audiofile(mp3)
+#         audioClip.close()
+#         videoClip.close()
+#         return wav, mp3
+#
+#     def convertToWav(wav, mp3):
+#         filepath = os.path.abspath(mp3)
+#         sound = AudioSegment.from_mp3(filepath).export(wav, format="wav")
+#
+#     def extractText(wav, fileName):
+#         r = sr.Recognizer()
+#         with sr.WavFile(os.path.abspath(wav)) as source:
+#             audio = r.record(source)
+#             text = r.recognize_google(audio)
+#             name = fileName.title()
+#             if ('_' in fileName):
+#                 name = name.replace('_', ' ')
+#             short_path = fileName + '.mp4'
+#         os.remove(fileName + '.mp3')
+#         os.remove(fileName + '.wav')
+#         return name, short_path, text
