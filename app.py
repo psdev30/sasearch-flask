2b294727e365a6b24bb5a38f6187b2ef968c5529
pszdev30 <pszjukalkar30@gmail.com>
2020-08-22 15:10:57 -0400

    retrieving all media files in cloudinary

--- app.py
+++ app.py
@@ -62,14 +62,18 @@ def landingPage():
 # add all clips in clips_library directory
 @app.route('/add_all_clips', methods=['POST', 'GET'])
 def add_clips_in_directory():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    for file in os.listdir(clipDirectory):
-        file = file.split('.')
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get('https://sasearch-backend.herokuapp.com/add_clip/{}'.format(file[0]))
-        # driver.get('http://127.0.0.1:5000/add_clip/{}'.format(file[0]))
-    return 'successfully added all clips!'
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    return cloudinary.api.resources(resource_type='video')
+
+
+    # for local use
+    # for file in os.listdir(clipDirectory):
+    #     file = file.split('.')
+    #     driver = webdriver.Chrome(options=chrome_options)
+    #     driver.get('https://sasearch-backend.herokuapp.com/add_clip/{}'.format(file[0]))
+    #     # driver.get('http://127.0.0.1:5000/add_clip/{}'.format(file[0]))
+    # return 'successfully added all clips!'
 
 
 @app.route('/random', methods=['GET'])
