33ab715c816d05585fa1e68b1c8ba791ef0bd546
pszdev30 <pszjukalkar30@gmail.com>
2020-08-22 16:30:45 -0400

    test changes

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ clipDirectory = 'C:/Users/psjuk/PyCharmProjects/SASearch-backend/clips_library/'
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = Options()
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 nltk.download('stopwords')
 
@@ -79,7 +79,7 @@ def add_clips_in_directory():
     cloudinary_response = cloudinary.api.resources(resource_type='video')
     for i in range(len(cloudinary_response['resources'])):
         public_id = cloudinary_response['resources'][i]['public_id']
-        driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get('https://sasearch-backend.herokuapp.com/add_clip/{}'.format(public_id[1]))
         # driver.get('https://sasearch-backend.herokuapp.com/add_clip/{}'.format(public_id[1]))
     return 'successfully added all clips!'
