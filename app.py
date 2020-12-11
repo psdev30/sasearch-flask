eb7c4623e96d3e61a53182a56757d3f42558a2ea
pszdev30 <pszjukalkar30@gmail.com>
2020-12-10 21:15:16 -0500

    moved cloudinary config back to main script

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ nltk.download('stopwords')
 app = Flask(__name__)
 CORS(app)
 
-config.cloudinary_config()
+# config.cloudinary_config()
+
+cloudinary.config(
+        cloud_name=config.CLOUD_NAME,
+        api_key=config.API_KEY,
+        api_secret=config.API_SECRET
+    )
 
 env = config.env
 
